d8f174a6fcad5daab17a909febc07a5452f751e4
Joseph Burnett <jburnett@gitlab.com>
2025-01-13 12:33:22 -0800

    Drop UI and write to JSON file

--- cmd/cgol/main.go
+++ cmd/cgol/main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
-	"image/color"
 	"log"
 	"os"
 	"time"
 
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"simonwaldherr.de/go/cgolGo/gif"
 	"simonwaldherr.de/go/cgolGo/life"
 	"simonwaldherr.de/go/golibs/gcurses"
@@ -99,60 +97,45 @@ func (gf *GifVisualizer) Complete() error {
 	return nil
 }
 
-// EbitenGame implements ebiten.Game interface for rendering.
-type EbitenGame struct {
-	field    *life.Field
-	cellSize int
-	tickRate time.Duration
-	lastTick time.Time
-	fps      int
+// JsonVisualizer handles JSON file creation.
+type JsonVisualizer struct {
+	filename string
+	frames   [][][]int
 }
 
-func (g *EbitenGame) Update() error {
-	if time.Since(g.lastTick) >= g.tickRate {
-		g.field = g.field.NextRound()
-		g.lastTick = time.Now()
+func NewJsonVisualizer(filename string) *JsonVisualizer {
+	return &JsonVisualizer{
+		filename: filename,
 	}
+}
+
+func (jv *JsonVisualizer) Setup() error {
 	return nil
 }
 
-func (g *EbitenGame) Draw(screen *ebiten.Image) {
-	cells := g.field.GetCells()
-	for y, row := range cells {
-		for x, cell := range row {
-			var clr color.Color
-			if cell != 0 {
-				clr = color.White
-			} else {
-				clr = color.Black
-			}
-			// Draw a rectangle for each cell
-			ebitenutil.DrawRect(screen, float64(x*g.cellSize), float64(y*g.cellSize), float64(g.cellSize), float64(g.cellSize), clr)
-		}
-	}
+func (jv *JsonVisualizer) AddFrame(cells [][]int) error {
+	jv.frames = append(jv.frames, cells)
+	return nil
 }
 
-func (g *EbitenGame) Layout(outsideWidth, outsideHeight int) (int, int) {
-	cells := g.field.GetCells()
-	height := len(cells)
-	width := 0
-	if height > 0 {
-		width = len(cells[0])
+func (jv *JsonVisualizer) Complete() error {
+	data, err := json.Marshal(jv.frames)
+	if err != nil {
+		return err
 	}
-	return width * g.cellSize, height * g.cellSize
+	return os.WriteFile(jv.filename, data, 0644)
 }
 
 // Global variables for flags
 var (
-	framesPerSecond   int
-	terminalWidth     int
-	terminalHeight    int
-	gameDuration      int
-	gifOutputLength   int
-	inputFilename     string
-	gifOutputFilename string
-	ebitenFlag        bool
-	ebitenCellSize    int
+	framesPerSecond    int
+	terminalWidth      int
+	terminalHeight     int
+	gameDuration       int
+	gifOutputLength    int
+	inputFilename      string
+	gifOutputFilename  string
+	jsonOutputFilename string
 )
 
 func initFlags() {
@@ -162,9 +145,8 @@ func initFlags() {
 	flag.IntVar(&framesPerSecond, "f", 20, "Frames per second")
 	flag.StringVar(&inputFilename, "o", "", "Input file to load initial state")
 	flag.StringVar(&gifOutputFilename, "g", "", "Output GIF filename")
+	flag.StringVar(&jsonOutputFilename, "j", "", "Output JSON filename")
 	flag.IntVar(&gifOutputLength, "l", 200, "Number of frames for GIF")
-	flag.BoolVar(&ebitenFlag, "e", false, "Enable Ebiten graphical output")
-	flag.IntVar(&ebitenCellSize, "s", 10, "Cell size for Ebiten visualization")
 	flag.Parse()
 }
 
@@ -190,70 +172,45 @@ func main() {
 	var visualizer Visualizer
 
 	switch {
-	case ebitenFlag:
-		// Ebiten visualization does not use the Visualizer interface
 	case gifOutputFilename != "":
 		visualizer = NewGifVisualizer(gifOutputFilename, gifOutputLength)
+	case jsonOutputFilename != "":
+		visualizer = NewJsonVisualizer(jsonOutputFilename)
 	default:
 		visualizer = NewTerminalVisualizer()
 	}
 
-	if !ebitenFlag {
+	if visualizer != nil {
+		err := visualizer.Setup()
+		if err != nil {
+			log.Fatalf("Failed to set up visualizer: %v", err)
+		}
+	}
+
+	// Determine the number of iterations
+	iterations := gameDuration
+	if iterations == -1 {
+		if gifOutputFilename != "" {
+			iterations = gifOutputLength
+		} else {
+			iterations = 1000 // Arbitrary large number for terminal
+		}
+	}
+
+	for i := 0; i < iterations; i++ {
 		if visualizer != nil {
-			err := visualizer.Setup()
+			err := visualizer.AddFrame(field.GetCells())
 			if err != nil {
-				log.Fatalf("Failed to set up visualizer: %v", err)
+				log.Fatalf("Failed to add frame: %v", err)
 			}
 		}
+		field = field.NextRound()
+	}
 
-		// Determine the number of iterations
-		iterations := gameDuration
-		if iterations == -1 {
-			if gifOutputFilename != "" {
-				iterations = gifOutputLength
-			} else {
-				iterations = 1000 // Arbitrary large number for terminal
-			}
-		}
-
-		for i := 0; i < iterations; i++ {
-			if visualizer != nil {
-				err := visualizer.AddFrame(field.GetCells())
-				if err != nil {
-					log.Fatalf("Failed to add frame: %v", err)
-				}
-			}
-			field = field.NextRound()
-		}
-
-		if visualizer != nil {
-			err := visualizer.Complete()
-			if err != nil {
-				log.Fatalf("Failed to complete visualizer: %v", err)
-			}
-		}
-	} else {
-		// Ebiten visualization
-		ebitenGame := &EbitenGame{
-			field:    field,
-			cellSize: ebitenCellSize,
-			tickRate: time.Second / time.Duration(framesPerSecond),
-			lastTick: time.Now(),
-			fps:      framesPerSecond,
-		}
-
-		// Calculate window size based on field dimensions
-		cells := ebitenGame.field.GetCells()
-		height := len(cells)
-		width := 0
-		if height > 0 {
-			width = len(cells[0])
-		}
-
-		ebiten.SetWindowSize(width*ebitenGame.cellSize, height*ebitenGame.cellSize)
-		ebiten.SetWindowTitle("Game of Life - Ebiten")
-		if err := ebiten.RunGame(ebitenGame); err != nil {
-			log.Fatalf("Ebiten failed: %v", err)
+	if visualizer != nil {
+		err := visualizer.Complete()
+		if err != nil {
+			log.Fatalf("Failed to complete visualizer: %v", err)
 		}
 	}
 }
